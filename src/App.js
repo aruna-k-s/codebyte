904c634f9b4cb79df21c8a505e2f0d39962c089a
Aruna k s <36861106+aruna-k-s@users.noreply.github.com>
2021-09-06 23:03:33 +0530

    Add files via upload
    codebyte

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/home';
+import Navbar from './components/navBar';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar/>
+        <Home ></Home>
+      </div>
+  );
+}
+
+export default App;
